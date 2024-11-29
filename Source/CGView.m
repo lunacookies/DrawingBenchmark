84b76ac8c483491b52ada2a8f41ed54c8dad209d
Luna Razzaghipour <luna@xoria.org>
2024-11-30 00:18:32 +1100

    Only re-shape text during layout in `CGView` and use text colors

--- Source/CGView.m
+++ Source/CGView.m
@@ -1,11 +1,19 @@
-@implementation CGView
+@interface CGView ()
+@property(nonatomic) CTFrameRef cachedCTFrame;
+@end
 
-- (void)drawRect:(NSRect)dirtyRect {
+@implementation CGView {
+	CTFrameRef _cachedCTFrame;
+}
+
+- (void)layout {
+	[super layout];
+
 	NSAttributedString *attributedString =
 	        [[NSAttributedString alloc] initWithString:@"Lorem ipsum dolor sit amet"
 	                                        attributes:@{
 		                                        NSFontAttributeName : [NSFont systemFontOfSize:NSFont.systemFontSize],
-		                                        NSForegroundColorAttributeName : NSColor.labelColor,
+		                                        NSForegroundColorAttributeName : NSColor.textColor,
 	                                        }];
 
 	CTFramesetterRef framesetter =
@@ -24,12 +32,35 @@
 	frameRect.size = frameSize;
 
 	CGPathRef path = CGPathCreateWithRect(frameRect, NULL);
-	CTFrameRef frame = CTFramesetterCreateFrame(framesetter, (CFRange){0}, path, NULL);
-	CTFrameDraw(frame, NSGraphicsContext.currentContext.CGContext);
+	self.cachedCTFrame = CTFramesetterCreateFrame(framesetter, (CFRange){0}, path, NULL);
 
 	CFRelease(path);
-	CFRelease(frame);
 	CFRelease(framesetter);
+}
+
+- (void)drawRect:(NSRect)dirtyRect {
+	[NSColor.textBackgroundColor set];
+	NSRectFill(dirtyRect);
+	CTFrameDraw(self.cachedCTFrame, NSGraphicsContext.currentContext.CGContext);
+}
+
+- (CTFrameRef)cachedCTFrame {
+	CFAutorelease(_cachedCTFrame);
+	return _cachedCTFrame;
+}
+
+- (void)setCachedCTFrame:(CTFrameRef)ctFrame {
+	if (_cachedCTFrame != NULL) {
+		CFRelease(_cachedCTFrame);
+	}
+	CFRetain(ctFrame);
+	_cachedCTFrame = ctFrame;
+}
+
+- (void)dealloc {
+	if (_cachedCTFrame != NULL) {
+		CFRelease(_cachedCTFrame);
+	}
 }
 
 @end
