a8060ef187e6b0c172fd6eb48259f53cc20633d3
Luna Razzaghipour <luna@xoria.org>
2024-12-09 21:54:46 +1100

    Allow user to edit text in CGView

--- Source/CGView.m
+++ Source/CGView.m
@@ -1,10 +1,27 @@
 @interface CGView ()
+@property(nonatomic) NSMutableArray<NSMutableString *> *lines;
 @property(nonatomic) CTFrameRef cachedCTFrame;
+@property(nonatomic) CGFloat cachedCTFrameHeight;
 @property(nonatomic) CGFloat scrollOffset;
+@property(nonatomic, readonly) NSUInteger currentLineIndex;
 @end
 
 @implementation CGView {
 	CTFrameRef _cachedCTFrame;
+}
+
+- (instancetype)init {
+	self = [super init];
+
+	NSURL *url = [NSBundle.mainBundle URLForResource:@"Text" withExtension:@"txt"];
+	NSString *contents = [NSString stringWithContentsOfURL:url encoding:NSUTF8StringEncoding error:nil];
+	self.lines = [[NSMutableArray alloc] init];
+	[contents enumerateLinesUsingBlock:^(NSString *line, BOOL *stop) {
+		(void)stop;
+		[self.lines addObject:[[NSMutableString alloc] initWithString:line]];
+	}];
+
+	return self;
 }
 
 - (void)scrollWheel:(NSEvent *)event {
@@ -12,11 +29,56 @@
 	self.needsDisplay = YES;
 }
 
+- (BOOL)acceptsFirstResponder {
+	return YES;
+}
+
+- (void)keyDown:(NSEvent *)event {
+	[self interpretKeyEvents:@[ event ]];
+}
+
+- (void)insertText:(id)stringAny {
+	NSAssert([stringAny isKindOfClass:[NSString class]], @"must be NSString");
+	NSString *string = (NSString *)stringAny;
+	[self.lines[self.currentLineIndex] appendString:string];
+	self.cachedCTFrame = NULL;
+	self.needsDisplay = YES;
+}
+
+- (void)insertNewline:(id)sender {
+	[self.lines insertObject:[[NSMutableString alloc] init] atIndex:self.currentLineIndex];
+	self.cachedCTFrame = NULL;
+	self.needsDisplay = YES;
+}
+
 - (void)layout {
 	[super layout];
+	self.cachedCTFrame = NULL;
+}
 
-	NSURL *url = [NSBundle.mainBundle URLForResource:@"Text" withExtension:@"txt"];
-	NSString *string = [NSString stringWithContentsOfURL:url encoding:NSUTF8StringEncoding error:nil];
+- (void)drawRect:(NSRect)dirtyRect {
+	NSGraphicsContext *context = NSGraphicsContext.currentContext;
+	[context saveGraphicsState];
+
+	[NSColor.textBackgroundColor set];
+	NSRectFill(self.bounds);
+
+	CGContextTranslateCTM(context.CGContext, 0, self.scrollOffset);
+	if (self.cachedCTFrame == NULL) {
+		[self updateCachedCTFrame];
+	}
+	CTFrameDraw(self.cachedCTFrame, NSGraphicsContext.currentContext.CGContext);
+
+	[context restoreGraphicsState];
+}
+
+- (void)updateCachedCTFrame {
+	NSMutableString *string = [[NSMutableString alloc] init];
+	for (NSMutableString *line in self.lines) {
+		[string appendString:line];
+		[string appendString:@"\n"];
+	}
+
 	NSAttributedString *attributedString = [[NSAttributedString alloc]
 	        initWithString:string
 	            attributes:@{
@@ -41,19 +103,10 @@
 
 	CGPathRef path = CGPathCreateWithRect(frameRect, NULL);
 	self.cachedCTFrame = CTFramesetterCreateFrame(framesetter, (CFRange){0}, path, NULL);
+	self.cachedCTFrameHeight = frameRect.size.height;
 
 	CFRelease(path);
 	CFRelease(framesetter);
-}
-
-- (void)drawRect:(NSRect)dirtyRect {
-	NSGraphicsContext *context = NSGraphicsContext.currentContext;
-	[context saveGraphicsState];
-	[NSColor.textBackgroundColor set];
-	NSRectFill(self.bounds);
-	CGContextTranslateCTM(context.CGContext, 0, self.scrollOffset);
-	CTFrameDraw(self.cachedCTFrame, NSGraphicsContext.currentContext.CGContext);
-	[context restoreGraphicsState];
 }
 
 - (CTFrameRef)cachedCTFrame {
@@ -68,6 +121,11 @@
 		CFRetain(ctFrame);
 	}
 	_cachedCTFrame = ctFrame;
+}
+
+- (NSUInteger)currentLineIndex {
+	CGFloat fractionThrough = ((self.scrollOffset + self.bounds.size.height / 2) / self.cachedCTFrameHeight);
+	return (NSUInteger)(fractionThrough * (CGFloat)self.lines.count);
 }
 
 - (void)dealloc {
